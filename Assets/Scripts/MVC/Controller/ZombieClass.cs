832b62ecebb65fdaf68e0fe77f09eb6ceb41e4fd
Matheus <matolisou@gmail.com>
2017-12-07 15:01:47 -0200

    Zombie things and improvements
    -Zombie class added;
    -Zombie movement added;
    -Zombie spawner added;
    -Zombie collision event added;
    -Some improvements

---
+++ Assets/Scripts/MVC/Controller/ZombieClass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieClass : MonoBehaviour {
+
+    public float speedZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        speedZ = 1;
+    }
+
+    void Movimentation()
+    {
+        transform.position -= new Vector3(0, 0, speedZ);
+        //GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -50));
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "car")
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movimentation();
+	}
+}
---
+++ Assets/Scripts/Managers/ZombieSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour {
+
+    public bool needGenerate;
+    public float timeToInstantiateZombie;
+    public float maxTimetoInstatiateZombie;
+    public float minPositionXToInstatiate;
+    public float maxPositionXToInstatiate;
+    public GameObject zombie;
+
+    // Use this for initialization
+    void Start()
+    {
+        needGenerate = false;
+        timeToInstantiateZombie = 0;
+        maxTimetoInstatiateZombie = 10f;
+        minPositionXToInstatiate = -15;
+        maxPositionXToInstatiate = 15;
+    }
+
+    void InstantiateZombies()
+    {
+        if (needGenerate)
+        {
+            Instantiate(zombie, new Vector3(Random.Range(minPositionXToInstatiate, maxPositionXToInstatiate), 0, 150), transform.rotation);
+            needGenerate = false;
+        }
+    }
+
+    void Timer()
+    {
+        if (!needGenerate)
+        {
+            timeToInstantiateZombie += 0.1f;
+        }
+        if (timeToInstantiateZombie >= maxTimetoInstatiateZombie)
+        {
+            needGenerate = true;
+            timeToInstantiateZombie = 0;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InstantiateZombies();
+        Timer();
+	}
+}
